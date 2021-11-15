e5f9e684ce3ecdb9683158493b1d1a865f34f75e
kakts <kakts.git@gmail.com>
2021-11-16 00:50:13 +0900

    [1-9.c] add strtol code

---
+++ ch01/1-9.c
@@ -0,0 +1,35 @@
+/**
+ * q1-9
+ * argv[0]以外のコマンドライン引数は全部数字と仮定する
+ * 
+ * これらの数字全部を合計した数を表示するプログラムを作る
+ * 文字列から整数の変換にはatoiかstrtolを使う
+ * 
+ * atoiは範囲外データを受け取ったときの扱いが不定のため、strtoiを使う
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h> // atoi
+
+int main(int argc, char *argv[]) {
+    if (argc < 1) {
+        exit(1);
+    }
+    int sum = 0;
+    for (int i = 1; i < argc; i++) {
+        int tmp_errno = errno;
+        errno = 0;
+        // 引数チェック用変数 整数に変換できない場合に、不正文字の先頭ポインタが返ってくる
+        char *check;
+        int data = strtol(argv[i], &check, 10);
+        if (*check != '\0') {
+            perror("strtoi: failed to convert");
+            exit(0);
+        }
+        sum += data;
+
+    }
+    printf("result: sum is %d \n", sum);
+}
+
--- ch01/arg.c
+++ ch01/arg.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * mainの引数確認用
+ * *argv []は **argvと書いても同じ
+ * 引数に配列を書いても、実際にはその配列の先頭要素へのポインタが渡されるから
+ * 
+ * argv[]の最後は必ずNULLなのでループの停止条件はargv[i] != NULLとかける
+ */
 int main (int argc, char *argv []) {
     int i;
     for (i = 0; i < argc; i++) {
