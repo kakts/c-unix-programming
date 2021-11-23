b4b0c5a81f89a8a7cd57234e4687fbcab81feb28
kakts <kakts.git@gmail.com>
2021-11-23 22:58:40 +0900

    [ch01/1-15.c] add any_add2 function code

--- ch01/1-15.c
+++ ch01/1-15.c
@@ -4,6 +4,7 @@
  * union any_t char int float double
  */
 #include <stdio.h>
+#include <assert.h> // assert
 
 union any_t {
     char c;
@@ -37,17 +38,56 @@ union any_t any_add(union any_t a1, union any_t a2, enum any_discr discr) {
             printf("not matched discr");
     }
     return ret;
-}
+};
 
-int main(void) {
+// any_t型とそのデータのdiscrをカプセル化したもの
+struct any_t2 {
+    enum any_discr discr;
+    union any_t u;
+};
+
+struct any_t2 any_add2 (struct any_t2 x, struct any_t2 y) {
+    struct any_t2 ret;
+    if (x.discr != y.discr) {
+        fprintf(stderr, "different type addition");
+        assert(0);
+    }
+    ret.discr = x.discr;
+    switch (x.discr) {
+    case ANY_CHAR:
+        ret.u.c = x.u.c + y.u.c;
+    case ANY_INT:
+        ret.u.i = x.u.i + y.u.i;
+    case ANY_FLOAT:
+        ret.u.f = x.u.f + y.u.f;
+    case ANY_DOUBLE:
+        ret.u.d = x.u.d + y.u.d;
+    default:
+        printf("not matched discr");
+    }
+    return ret;
+};
+
+int main(void)
+{
 
     union any_t a1, a2;
     a1.i = 10;
     a2.i = 20;
 
+    struct any_t2 x, y;
+    x.discr = ANY_INT;
+    y.discr = ANY_INT;
+    x.u = a1;
+    y.u = a2;
+
     union any_t res;
     enum any_discr discr = ANY_INT;
     res = any_add(a1, a2, discr);
 
-    printf("result. discr: %d, data: %d", discr, res.i);
+    printf("result. discr: %d, data: %d\n", discr, res.i);
+
+    struct any_t2 res_t2;
+    res_t2 = any_add2(x, y);
+    printf("result any_t2. discr: %d, data: %d\n", res_t2.discr, res_t2.u.i);
 }
