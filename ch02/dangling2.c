a2af941273103254012deb0a12b05e57b7fc3317
kakts <kakts.git@gmail.com>
2021-11-25 01:31:47 +0900

    [q2-7] add dangling pointer fixing

---
+++ ch02/dangling2.c
@@ -0,0 +1,37 @@
+/**
+ * @file 2-7.c
+ * 
+ * dangling pointerの解消
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {
+    int x, y;
+};
+
+/**
+ * dangling pointerが存在しないバージョン
+ */
+int wrong_main(void)
+{
+    struct point *p = malloc(sizeof(struct point));
+    int *q = &p->x;
+    p->x = 10; p->y = 20;
+    printf("%d\n", *q);
+    free(p);
+}
+
+/**
+ * dangling pointerが存在する
+ */
+int wrong_main(void)
+{
+    struct point *p = malloc(sizeof(struct point));
+    int *q = &p->x;
+    p->x = 10; p->y = 20;
+    free(p);
+    // pをfreeした後にpのメンバ変数xへのポインタを参照している
+    printf("%d\n", *q);
+}
