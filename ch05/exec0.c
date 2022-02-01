5617187baa74b144993fdbf020c225b42d3f074a
kakts <kakts.git@gmail.com>
2022-02-01 22:58:29 +0900

    [ch05/5.10] add mysh0.c for implementing simple shell program

--- ch05/exec0.c
+++ ch05/exec0.c
@@ -12,6 +12,10 @@
 int main(void)
 {
     printf("Invoking ls.\n");
+    /**
+     * exec関数は、呼び出しプロセスのメモリ領域の内容を、新しいプログラムを実行するための内容で上書きしてしまう
+     * exec呼び出し時点でstdioライブラリのバッファに残っているデータは、呼び出しが成功すると捨てられてしまう。
+     */
     fflush(stdout);
     execlp("ls", "ls", (char *) 0);
 
